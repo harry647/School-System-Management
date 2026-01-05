e04975e246889ce90a713abf94709ccb641ad1aa
harry647 <harryoginga@gmail.com>
2026-01-05 18:20:11 +0300

    update the __init__.py

--- school_system/services/__init__.py
+++ school_system/services/__init__.py
@@ -1,2 +1,24 @@
 # Initialize the services package
 # This file can be used to import all services for easier access
+
+from .auth_service import AuthService
+from .student_service import StudentService
+from .teacher_service import TeacherService
+from .book_service import BookService
+from .furniture_service import FurnitureService
+from .qr_service import QRService
+from .report_service import ReportService
+from .import_export_service import ImportExportService
+from .notification_service import NotificationService
+
+__all__ = [
+    'AuthService',
+    'StudentService',
+    'TeacherService',
+    'BookService',
+    'FurnitureService',
+    'QRService',
+    'ReportService',
+    'ImportExportService',
+    'NotificationService'
+]
